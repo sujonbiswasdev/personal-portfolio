49925e110e70737153575f717625354fad2fad04
sujonbiswasdev <sujonbiswasdev@gmail.com>
2025-12-17 03:01:03 +0600

    done

--- src/components/ui/images-slider.tsx
+++ src/components/ui/images-slider.tsx
@@ -95,7 +95,7 @@ export const ImagesSlider = ({
       opacity: 1,
       transition: {
         duration: 0.5,
-        ease: [0.645, 0.045, 0.355, 1.0],
+        ease: [0.645, 0.045, 0.355, 1.0] as const,
       },
     },
     upExit: {
